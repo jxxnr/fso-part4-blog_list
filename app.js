e863c0bf434bcdc17e99444ed4ca7661aa013e18
jxxnr <ramilojeanlouise@gmail.com>
2021-10-10 21:50:02 +0800

    Add ESLint, set additional linting rules

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ mongoose.connect(mongoUrl)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
-  app.use(cors())
-  app.use(express.static('build'))
-  app.use(express.json())
-  app.use(middleware.requestLogger)
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 
-  app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter)
 
-  app.use(middleware.unknownEndpoint)
-  app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-  module.exports = app
+module.exports = app
